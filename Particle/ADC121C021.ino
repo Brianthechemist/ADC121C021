01481b5f2c1ab0fe36161af41bce8b223aea60ae
Yadwinder Singh <ryker1990@gmail.com>
2016-06-20 14:08:41 +0530

    Update ADC121C021.ino

--- Particle/ADC121C021.ino
+++ Particle/ADC121C021.ino
@@ -42,14 +42,13 @@ void loop()
     {  
         data[0] = Wire.read();
         data[1] = Wire.read();
-    
-    delay(300);
+    }
     
     // Convert the data to 12 bits
     raw_adc = ((data[0] * 256) + data[1]) & 0x0FFF;
 
     // Output data to dashboard
     Particle.publish("Digital value of analog input: ", String(raw_adc));
-    }
+    delay(1000);
 }
 
